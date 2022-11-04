d7a58dc3e2d8127d11ab9204460a39e160914a01
fe-ktch <sangarefantha@gmail.com>
2022-11-04 10:18:33 +0100

    ts added

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const calcButton = document.querySelector('#calcButton');
+const result = document.querySelector('#output');
+calcButton === null || calcButton === void 0 ? void 0 : calcButton.addEventListener('click', () => {
+    const sugar = document.querySelector('#sugar');
+    const length = document.querySelector('#length');
+    let numSugar = Number(sugar.value);
+    let numLength = Number(length.value);
+    let surface = calcSurface(numSugar, numLength);
+    result.textContent = String(surface);
+    sugar.value = "";
+    length.value = "";
+});
+function calcSurface(sugar, length) {
+    return 2 * Math.PI * Math.pow(sugar, 2) + 2 * Math.PI * sugar * length;
+}
---
+++ src/app.ts
@@ -0,0 +1,20 @@
+const calcButton = document.querySelector('#calcButton');
+const result = document.querySelector('#output');
+
+calcButton?.addEventListener('click', () => {
+    const sugar = document.querySelector('#sugar') as HTMLInputElement;
+    const length = document.querySelector('#length') as HTMLInputElement;
+
+    let numSugar: number = Number(sugar.value);
+    let numLength: number = Number(length.value);
+
+    let surface = calcSurface(numSugar, numLength);
+    result.textContent = String(surface);
+    sugar.value = "";
+    length.value = "";
+});
+
+
+function calcSurface(sugar: number, length: number):number {
+    return 2 * Math.PI * Math.pow(sugar, 2) + 2 * Math.PI * sugar * length
+}
